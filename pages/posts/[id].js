2ccc9cc12d7e09e57511dd895c0ce9e05d5a1c82
GomaGoma676 <pga.adam.scott@gmail.com>
2021-01-23 13:34:34 +0900

    fixed object type

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -49,7 +49,8 @@ export async function getStaticPaths() {
   };
 }
 export async function getStaticProps({ params }) {
-  const { post: post } = await getPostData(params.id);
+  //const { post: post } = await getPostData(params.id);
+  const post = await getPostData(params.id);
   return {
     props: {
       post,
